b7c54609ea06fc153ebaf5149a85f9bf188df461
zeroprin-zf <zerozerozf07@outlook.jp>
2024-03-29 11:39:29 +0900

    拡張for文

---
+++ src/array2/java/array5_another.java
@@ -0,0 +1,14 @@
+package array2.java;
+
+public class array5_another {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		int[] scores = {20, 30, 40, 50, 80};
+		for (int value : scores) {
+			System.out.println(value);
+		}
+	}
+
+}
